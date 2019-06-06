ce8bf49616c0a5e212cf26c92a2da7ed788f5cdc
marysa <marysa31g@gmail.com>
2019-06-05 20:39:52 -0500

    view

---
+++ view_videos.php
@@ -0,0 +1,149 @@
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<script type="text/javascript"  src="js/bootstrap.js"></script>
+<script type="text/javascript"  src="js/jquery-3-3-1.js"></script>
+<!--JS y CSS-->
+<section class="jumbotron text-center p-3 mb-2 bg-primary">
+  <div class="container">
+    <h1 class="jumbotron-heading">Rutinas </h1>
+    <p class="card-text"></p>
+
+  </p>
+</div>
+</section>
+
+
+<div class="album py-5">
+  <div class="promo box ">
+    <div class="row">
+      <!--Video 1-->
+      <div class="col-md-4">
+        <h2 class="card-title"></h2>
+        <div class="card mb-4 shadow-sm">
+          <iframe class="card-img-top" width="100%" height="225" src="https://www.youtube.com/embed/nKmH2Laeirk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <div class="card-body text-info">
+            <h3 class="card-title">Rutina intensa y matadora de piernas </h3>
+            <h5 class="card-title">Categoria Pierna</h5>
+            <h5 class="text-muted" ><em>En este tutorial aprenderás a realizar sentadillas</em></h5>
+            <div class="card-footer">
+              <small class="text-muted">Ultima actualización: 05/06/2019</small>
+            </div>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <a href=" " role="button" class="btn btn-sm btn-outline-secondary">Ver más</a>
+              </div>
+              <small class="text-muted">20:25 mins</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--Video 2-->
+      <div class="col-md-4">
+        <h2 class="card-title"></h2>
+        <div class="card mb-4 shadow-sm">
+          <iframe class="card-img-top" width="100%" height="225" src="https://www.youtube.com/embed/nKmH2Laeirk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <div class="card-body text-info">
+            <h3 class="card-title">Rutina intensa y matadora de piernas </h3>
+            <h5 class="card-title">Categoria Pierna</h5>
+            <h5 class="text-muted" ><em>En este tutorial aprenderás a realizar sentadillas</em></h5>
+            <div class="card-footer">
+              <small class="text-muted">Ultima actualización: 05/06/2019</small>
+            </div>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <a href=" " role="button" class="btn btn-sm btn-outline-secondary">Ver más</a>
+              </div>
+              <small class="text-muted">20:25 mins</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--Video 3-->
+     <div class="col-md-4">
+        <h2 class="card-title"></h2>
+        <div class="card mb-4 shadow-sm">
+          <iframe class="card-img-top" width="100%" height="225" src="https://www.youtube.com/embed/nKmH2Laeirk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <div class="card-body text-info">
+            <h3 class="card-title">Rutina intensa y matadora de piernas </h3>
+            <h5 class="card-title">Categoria Pierna</h5>
+            <h5 class="text-muted" ><em>En este tutorial aprenderás a realizar sentadillas</em></h5>
+            <div class="card-footer">
+              <small class="text-muted">Ultima actualización: 05/06/2019</small>
+            </div>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <a href=" " role="button" class="btn btn-sm btn-outline-secondary">Ver más</a>
+              </div>
+              <small class="text-muted">20:25 mins</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--Video 4-->
+     <div class="col-md-4">
+        <h2 class="card-title"></h2>
+        <div class="card mb-4 shadow-sm">
+          <iframe class="card-img-top" width="100%" height="225" src="https://www.youtube.com/embed/nKmH2Laeirk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <div class="card-body text-info">
+            <h3 class="card-title">Rutina intensa y matadora de piernas </h3>
+            <h5 class="card-title">Categoria Pierna</h5>
+            <h5 class="text-muted" ><em>En este tutorial aprenderás a realizar sentadillas</em></h5>
+            <div class="card-footer">
+              <small class="text-muted">Ultima actualización: 05/06/2019</small>
+            </div>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <a href=" " role="button" class="btn btn-sm btn-outline-secondary">Ver más</a>
+              </div>
+              <small class="text-muted">20:25 mins</small>
+            </div>
+          </div>
+        </div>
+      </div>
+       <!--Video 5-->
+     <div class="col-md-4">
+        <h2 class="card-title"></h2>
+        <div class="card mb-4 shadow-sm">
+          <iframe class="card-img-top" width="100%" height="225" src="https://www.youtube.com/embed/nKmH2Laeirk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <div class="card-body text-info">
+            <h3 class="card-title">Rutina intensa y matadora de piernas </h3>
+            <h5 class="card-title">Categoria Pierna</h5>
+            <h5 class="text-muted" ><em>En este tutorial aprenderás a realizar sentadillas</em></h5>
+            <div class="card-footer">
+              <small class="text-muted">Ultima actualización: 05/06/2019</small>
+            </div>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <a href=" " role="button" class="btn btn-sm btn-outline-secondary">Ver más</a>
+              </div>
+              <small class="text-muted">20:25 mins</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--Video 6-->
+     <div class="col-md-4">
+        <h2 class="card-title"></h2>
+        <div class="card mb-4 shadow-sm">
+          <iframe class="card-img-top" width="100%" height="225" src="https://www.youtube.com/embed/nKmH2Laeirk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <div class="card-body text-info">
+            <h3 class="card-title">Rutina intensa y matadora de piernas </h3>
+            <h5 class="card-title">Categoria Pierna</h5>
+            <h5 class="text-muted" ><em>En este tutorial aprenderás a realizar sentadillas</em></h5>
+            <div class="card-footer">
+              <small class="text-muted">Ultima actualización: 05/06/2019</small>
+            </div>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <a href=" " role="button" class="btn btn-sm btn-outline-secondary">Ver más</a>
+              </div>
+              <small class="text-muted">20:25 mins</small>
+            </div>
+          </div>
+        </div>
+      </div>
+
+ </div>
+</div>
+</div>
+
+
